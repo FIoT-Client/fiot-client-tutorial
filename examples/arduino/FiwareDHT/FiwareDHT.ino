3f6b3531f6636f14e7e7a4b30dee5885b14f9646
Lucas Cristiano Calixto Dantas <lucascristiano27@gmail.com>
2017-08-25 16:42:37 -0300

    Update arduino example

--- examples/arduino/FiwareDHT/FiwareDHT.ino
+++ examples/arduino/FiwareDHT/FiwareDHT.ino
@@ -3,6 +3,15 @@
 #include <PubSubClient.h>
 #include <dht.h>
 
+#define DHT21_PIN 2
+#define LED_PIN   3
+
+#define ERROR_DELAY   2000
+#define SUCCESS_DELAY 60000
+
+unsigned long lastEventTime;
+boolean sendMeasurementSuccess;
+
 // Update these with values suitable for your network.
 byte mac[] = { 0xDE, 0xED, 0xBA, 0xFE, 0xFE, 0xED };
 IPAddress server(10, 7, 49, 163);
@@ -15,40 +24,18 @@ IPAddress gateway(10, 10, 1, 1);
 IPAddress subnet(255, 255, 255, 0);
 **/
 
-#define DHT21_PIN 2
-#define LED_PIN   3
-
-// Callback function header
-void callback(char* topic, byte* payload, unsigned int length);
+void callback(char* topic, byte* payload, unsigned int length);  // Callback function header
 
 EthernetClient ethClient;
-PubSubClient client(ethClient);
+PubSubClient client(server, 1883, callback, ethClient);
 
 dht DHT;
 
-char* cmdAnswerTopic = "/4jggokgpepnvsb2uv4s40d59ov2/STELA_LED/cmdexe";
-
-void callback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Message arrived [");
-  Serial.print(topic);
-  Serial.println("] ");
-
-  for (int i=0; i < length; i++) {
-    Serial.print((char)payload[i]);
-  }
-
-  if (strcmp((char *) payload, "change_state|ON") == 0) {
-    digitalWrite(LED_PIN, HIGH);
-    client.publish(cmdAnswerTopic, "change_state|Done");
-  } else if (strcmp((char *) payload, "change_state|OFF") == 0) {
-    digitalWrite(LED_PIN, LOW);
-    client.publish(cmdAnswerTopic, "change_state|Done");
-  } else {
-    client.publish(cmdAnswerTopic, "change_state|Error");
-  }
-
-  Serial.println("Answer sent");
-}
+const char* cmdAnswerTopic = "/2fb0f72689c611e7bb5860f81db4b630/STELA_LED/cmdexe";
+const char* payloadOn = "STELA_LED@change_state|ON";
+const char* payloadOff = "STELA_LED@change_state|OFF";
+const char* payloadOk = "STELA_LED@change_state|OK";
+const char* payloadError = "STELA_LED@change_state|ERROR";
 
 void setup() {
   Serial.begin(115200);
@@ -56,12 +43,64 @@ void setup() {
 
   pinMode(LED_PIN, OUTPUT);
 
-  client.setServer(server, 1883);
-  
   Ethernet.begin(mac); //DHCP (auto IP)
   //Ethernet.begin(mac, ip, localDns, gateway, subnet); //Manual IP
-  
+  printIPAddress();
+
   delay(1500);  //Allow the hardware to sort itself out
+
+  lastEventTime = millis();
+  sendMeasurementSuccess = false;
+}
+
+void printIPAddress() {
+  Serial.print("IP address: ");
+  for (byte thisByte = 0; thisByte < 4; thisByte++) {
+    Serial.print(Ethernet.localIP()[thisByte], DEC);
+    Serial.print(".");
+  }
+  Serial.println();
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.println("] ");
+
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  if (checkPayloadEqual(payloadOn, (char*)payload, length)) {
+    Serial.println("ON");
+    digitalWrite(LED_PIN, HIGH);
+    client.publish(cmdAnswerTopic, payloadOk);
+
+  } else if (checkPayloadEqual(payloadOff, (char*)payload, length)) {
+    Serial.println("OFF");
+    digitalWrite(LED_PIN, LOW);
+    client.publish(cmdAnswerTopic, payloadOk);
+
+  } else {
+    Serial.println("UNKNOWN");
+    client.publish(cmdAnswerTopic, payloadError);
+  }
+
+  Serial.println("Answer sent");
+}
+
+boolean checkPayloadEqual(const char* payload, char* payloadInput, unsigned int inputPayloadLength) {
+  if (strlen(payload) != inputPayloadLength) {
+    return false;
+  }
+
+  for(int i = 0; i < inputPayloadLength; i++) {
+    if(payload[i] != payloadInput[i]) {
+      return false;
+    }
+  }
+  return true;
 }
 
 void publishMeasurements() {
@@ -79,21 +118,33 @@ void publishMeasurements() {
       sprintf(measurementPayloadBuf, "h|%s|t|%s", strHumidity, strTemperature);
       Serial.println(measurementPayloadBuf);
 
-      client.publish("/4jggokgpepnvsb2uv4s40d59ov2/STELA_DHT/attrs", measurementPayloadBuf);
+      client.publish("/2fb0f72689c611e7bb5860f81db4b630/STELA_DHT/attrs", measurementPayloadBuf);
 
-      delay(60000);
+      Serial.println("Mesurement sent!");
+      sendMeasurementSuccess = true;
       break;
+
     default:
-      Serial.println("Failed to read sensor. Trying again in 2 seconds.");
-      delay(2000);
+      Serial.println("Failed to read sensor. Trying again later.");
+      sendMeasurementSuccess = false;
       break;
   }
+  lastEventTime = millis();
 }
 
 void reconnect() {
   while (!client.connected()) {
     Serial.println("Connecting to server...");
-    if (!client.connect("arduinoClient")) {
+    if (client.connect("arduinoClient")) {
+      Serial.println("Connected!");
+
+      Serial.println("Subscribing LED command topic...");
+      if(client.subscribe("/2fb0f72689c611e7bb5860f81db4b630/STELA_LED/cmd")) {
+        Serial.println("Successfully subscribed to topic!");
+      } else {
+        Serial.println("Error while subscribing to topic.");
+      }
+    } else {
       Serial.println("Connection failed. Trying again in 2 seconds.");
       delay(2000); // Wait until try again
     }
@@ -101,10 +152,15 @@ void reconnect() {
 }
 
 void loop() {
-  if (client.connected()){
-    Serial.println("Subscribing LED command topic");
-    client.subscribe("/4jggokgpepnvsb2uv4s40d59ov2/STELA_LED/cmd");
-    publishMeasurements();
+  if (client.connected()) {
+    unsigned long currentTime = millis();
+    unsigned long elapsedTime = abs(currentTime - lastEventTime);
+
+    if ((sendMeasurementSuccess == true && elapsedTime >= SUCCESS_DELAY)
+        || (sendMeasurementSuccess == false && elapsedTime >= ERROR_DELAY)) {
+      publishMeasurements();
+    }
+    client.loop();
   } else {
     reconnect();
   }
