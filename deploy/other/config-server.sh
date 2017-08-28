a5ca4f85c2d5d2c59f4dca75f6a09f90f74e3220
Lucas Cristiano Calixto Dantas <lucascristiano27@gmail.com>
2017-08-28 18:12:30 -0300

    Change and reorganization deploy files

--- deploy/other/config-server.sh
+++ deploy/other/config-server.sh
@@ -5,12 +5,15 @@
 # https://askubuntu.com/questions/59458/error-message-when-i-run-sudo-unable-to-resolve-host-none
 
 # Docker installation
+sudo apt-get update
 sudo apt-get -y install apt-transport-https ca-certificates curl
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+# Get system arch: https://unix.stackexchange.com/questions/180726/easy-command-line-method-to-determine-specific-arm-architecture-string
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+# sudo add-apt-repository "deb [arch=armhf] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+# sudo add-apt-repository "deb [arch=s390x] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
 sudo apt-get -y install docker-ce
-sudo apt install docker-compose
+sudo apt-get -y install docker-compose
 sudo usermod -aG docker ${USER}
 sudo reboot
-
